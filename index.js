ad4ccd499f998af75211744525336006bc1ceec9
RUKICUA <bxv.se.gd@gmail.com>
2021-07-15 14:12:13 +0700

    remove firebase

--- index.js
+++ index.js
@@ -1,22 +1,8 @@
 const express = require('express');
 const app = express();
-const firebase = require('firebase');
-require("firebase/firestore");
+
 const bodyParser = require('body-parser');
 const { json } = require('express');
-
-var firebaseConfig = {
-    apiKey: "AIzaSyBAw9tm7xpRzM1RqkABJwlky4IRk4bOey4",
-    authDomain: "learn-c30eb.firebaseapp.com",
-    projectId: "learn-c30eb",
-    storageBucket: "learn-c30eb.appspot.com",
-    messagingSenderId: "369947124414",
-    appId: "1:369947124414:web:ca0a5c6bb9e8911463f4ef"
-  };
-
-firebase.initializeApp(firebaseConfig);
-
-const db = firebase.firestore();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
