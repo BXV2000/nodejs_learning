12db88311fe8587d907de1f9ff058fb66fe69139
william-huynh <hnbaolam@gmail.com>
2021-07-16 14:51:00 +0700

    connect with mysql

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
-var express = require('express');
-var app = express();
-
+let express = require('express');
+let app = express();
+let mysql = require('mysql');
+let con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "monsterhunter"
+});
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
+
+// app.get('/public/home.html', function (req, res) {              check connection
+//     var sql = "SELECT * FROM surveyform";
+//     con.query(sql, function(err, results) {
+//       if (err) throw err;
+//       res.send(results);
+//     });
+// });
 
 app.get("/", (req, res) => {
     res.render("hello")
