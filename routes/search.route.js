8a12f3297ca4b11877f6fc0680163cb7c41e4de1
RUKICUA <bxv.se.gd@gmail.com>
2021-07-28 08:54:33 +0700

    fix some syncing

--- routes/search.route.js
+++ routes/search.route.js
@@ -15,8 +15,10 @@ router.get('/', function(req, res) {
 });
 
 router.post('/', function(req, res) {
-  let sql='SELECT * FROM surveyform WHERE isActive=1';
-  db.query(sql, function (err, data) { 
+  let id = req.body.searchID;
+  if(id===""){
+    let sql='SELECT * FROM surveyform WHERE isActive=1';
+    db.query(sql, function (err, data) { 
     if (err) {
       console.log(err);
       res.status(500).json({message: 'Hello there!'});
@@ -24,34 +26,45 @@ router.post('/', function(req, res) {
     console.log("Search db connected");
     res.render('search', {profile:data});
   });
+  }
+  else{
+    let sql=`SELECT * FROM surveyform WHERE isActive=1 AND id='${id}'`;
+    db.query(sql, function (err, data) { 
+      if (err) {
+        res.status(500).json({message: 'Hello there!'});
+        throw err;
+      };
+      console.log("Search db connected");
+      res.render('search', {profile:data});
+    });
+  }
 });
 
 router.delete('/',function(req,res){
   let id = req.body.id;
-  let sql=`UPDATE surveyform SET isActive=0 WHERE id=${id}`;
+  let sql=`UPDATE surveyform SET isActive=0 WHERE id='${id}'`;
   db.query(sql, function (err, data) {
     if (err) {
       console.log(err);
       res.status(500).json({message: 'Hello there!'});
     };
     console.log("Deleted!!");
+    res.redirect("/search");
     res.render('search', {profile:data});
-    
   });
-  res.redirect("/search");
 })
 
 router.put('/',function(req, res){
   let changeHunterName = req.body.changeHunterName;
   let changeHunterEmail= req.body.changeHunterEmail;
   let id = req.body.id;
-  let sql=`UPDATE surveyform SET hunterName='${changeHunterName}', email='${changeHunterEmail}' WHERE id=${id}`;
+  let sql=`UPDATE surveyform SET hunterName='${changeHunterName}', email='${changeHunterEmail}' WHERE id='${id}'`;
   db.query(sql, function (err, data) {
     if (err) throw err;
     console.log("Changed!!!");
+    res.redirect("/search");
     res.render('search', {profile:data});
   });
-  res.redirect("/search");
 })
 
 module.exports = router;
